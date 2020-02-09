6050a426aaeac37ac418a4dcc64d52bfc86850c2
GaryLocke91 <garylocke@outlook.com>
2020-02-09 15:04:36 +0000

    Updated files with comments

--- collatz.py
+++ collatz.py
@@ -1,9 +1,17 @@
+#This program asks the user to input any positive interger and outputs the successive values following a particular calculation
+
+#Asks the user to input a positive interger
 user_input = input("Please enter a positive integer: ")
+#Assigns the number to a variable and prints it
 num = int(user_input)
 print(num)
+#Runs a while loop until the value of the variable is 1
 while num > 1:
+    #If the value is even, it is divided by 2
     if (num % 2) == 0:
         num = int(num/2)
+    #Otherwise, the value is multiplied by 3 and added to 1
     else:
         num = (num * 3) + 1
+    #Prints the number following each iteration
     print(int(num))
--- secondstring.py
+++ secondstring.py
@@ -1,2 +1,6 @@
+#This program asks the user to input a string and output every second letter in reverse order
+
+#Asks the user to input a sentence
 string_input = input("Please enter a sentence: ")
+#Returns every second letter in the string in reverse order
 print(string_input[::-2])
