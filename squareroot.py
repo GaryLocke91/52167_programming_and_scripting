8843f6fce43726f6bed906b06463ad729bf37f67
GaryLocke91 <garylocke@outlook.com>
2020-03-14 23:36:15 +0000

    Updated based on feedback - use of Newton's Method

--- squareroot.py
+++ squareroot.py
@@ -5,10 +5,21 @@ import math
 #Asks the user to input a positive number
 num = float(input("Please enter a positive number: "))
 
-#Defines the function which takes one input and uses the appropriate method in the math module
+#Defines the function (sourced from https://helloacm.com)
 def sqrt(x):
-    ans = math.sqrt(x)
-    return(ans)
+    sgn = 0
+    if x < 0:
+        sgn = -1
+        x = -x
+    val = x
+    while True:
+        last = val
+        val = (val + x / val) * 0.5
+        if abs(val - last) < 1e-9:
+            break
+    if sgn < 0:
+        return complex(0, val)
+    return(val)
 
 #Calls the function and assigns it to a variable
 sqrt_num = sqrt(num)
