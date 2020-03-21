d94ac6958cffd4e4fa2058df316a698096f81fa3
GaryLocke91 <garylocke@outlook.com>
2020-03-21 18:27:37 +0000

    Updated based on feedback - removed word splitting

--- es.py
+++ es.py
@@ -7,11 +7,8 @@ e_count = 0
 #Opens and reads the file, and stores it in a variable
 with open(file_name) as f:
     read_data = f.read()
-    #Loops through each line, splits the line into words and stores them in a list
-    for line in read_data:
-        words = line.split()
-        #Loops through each character of each word and increments the count variable
-        for i in words:
-            if 'e' in i or 'E' in i:
-                e_count += 1               
+    #Loops through each character of each word and increments the count variable
+    for i in read_data:
+        if i == 'e' or i == 'E':
+            e_count += 1               
 print(e_count)
